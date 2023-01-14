629d3f2d6b8451235c4a992e9419915f1eae89e5
andresdavidsv <soy@andresdavidsv.com>
2023-01-14 16:54:34 -0500

    feat: learn flutter

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const fontSize30 = TextStyle(fontSize: 30);
+    int counter = 10;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Number of Clicks', style: fontSize30),
+            Text('$counter', style: fontSize30),
+          ],
+        ),
+      ),
+      // floatingActionButtonLocation:
+      //     FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print('Hola mundo'), counter++},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
